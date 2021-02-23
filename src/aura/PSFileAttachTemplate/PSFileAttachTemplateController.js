0a8d909edfa29cbd7eda871cc2d8c488e0a35755
Troy Hedges <thedges@salesforce.com>
2021-02-23 14:08:27 -0600

    Update sort order

--- src/aura/PSFileAttachTemplate/PSFileAttachTemplateController.js
+++ src/aura/PSFileAttachTemplate/PSFileAttachTemplateController.js
@@ -2,5 +2,9 @@
 	doInit : function(component, event, helper) {
 		console.log('PSFileAttachTemplateController::doInit...');
 		helper.getFileDefs(component);
+	},
+	handleRecordUpdated: function (component, event, helper) {
+		console.log('PSFileAttachTemplateController::handleRecordUpdated...');
+		helper.getFileDefs(component);
 	}
 })
